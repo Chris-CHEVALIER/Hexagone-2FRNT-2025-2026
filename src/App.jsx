b7b22839c933996941e381da69eea157be697543
Chris <chevalier@chris-freelance.com>
2026-03-16 14:02:41 +0100

    clear example

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
 import './App.css'
-import Button from './components/Button'
-import Lorem from './components/Lorem'
 import Welcome from './components/Welcome'
 
 export default function App () {
-  const students = [
-    { id: 1, firstName: 'Mélodie', lastName: 'REY' },
-    { id: 2, firstName: 'Florian', lastName: 'LECHEVALLIER' },
-    { id: 3, firstName: 'Eladji', lastName: 'FOFANA' }
-  ]
-
   return (
     <>
-      <Lorem />
-      <Button />
-      {students.map(student => (
-        <Welcome
-          key={student.id}
-          firstName={student.firstName}
-          lastName={student.lastName}
-        />
-      ))}
+      <Welcome firstName='Chris' lastName='CHEVALIER' />
     </>
   )
 }
--- src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-export default function Button() {
-  return (
-    <button>Mon super bouton</button>
-  )
-}
